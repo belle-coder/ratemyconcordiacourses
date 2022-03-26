c48e0ed64531da5f9f08119a135319554098bec0
bhattirahat <48467256+bhattirahat@users.noreply.github.com>
2022-03-26 16:31:56 -0400

    added something

--- screens/Questionnaire.tsx
+++ screens/Questionnaire.tsx
@@ -1,28 +1,25 @@
 import React from "react";
-import styles, { DARK_GRAY } from "../assets/styles";
-
-
-import {TouchableOpacity, View,Text,StyleSheet } from "react-native";
-import Icon from "../components/Icon";
+import styles from "../assets/styles";
+import {TouchableOpacity, View,Text,StyleSheet,TextInput } from "react-native";
 import RNPickerSelect from 'react-native-picker-select';
-
-import { blue100 } from "react-native-paper/lib/typescript/styles/colors";
-import { ScrollView } from "react-native-gesture-handler";
-
-
+import { ScrollView, } from "react-native-gesture-handler";
 
 const Questionnaire = ({ navigation } : any) => {
     
-  
-      
   return (
     <ScrollView>
-    <View style={{marginTop :130}}>
+    <View style={{marginTop :60}}>
+    <View>
+        <Text style = {{marginBottom : 70,fontSize : 22, marginLeft : 30,}}>
+            Questionnaire
+        </Text>
+    </View>
+
+    <View style = {{height : 10}}></View>
+    
     <View style={styles.containerProfileItem}>
-        <View style={styles.top}>
-        <Text style={styles.title}>Questionnaire</Text>
-        </View>
-        <Text style={styles.containerHome}>
+        
+        <Text style={pickerSelectStyles.question}>
          [Question 1 will be here]
         </Text>
         <View style = {{marginLeft:10,marginTop: 10}} >
@@ -32,11 +29,11 @@ const Questionnaire = ({ navigation } : any) => {
         onValueChange={(value) => (value)}
         items={[
             { label: 'Yes', value: 'yes' },
-            { label: 'No', value: 'baseball' },
+            { label: 'No', value: 'no' },
         ]} />
         </View>
       
-        <Text style={styles.containerHome}>
+        <Text style={pickerSelectStyles.question}>
          [Question 1 will be here]
         </Text>
         <View style = {{marginLeft:10,marginTop: 10}} >
@@ -45,13 +42,13 @@ const Questionnaire = ({ navigation } : any) => {
         style={pickerSelectStyles}
         onValueChange={(value) => (value)}
         items={[
-            { label: 'Yes', value: 'yes' },
-            { label: 'No', value: 'baseball' },
+            { label: 'Yes', value: 'yes'},
+            { label: 'No', value: 'no'  },
         ]} />
         </View>
     
-        <Text style={styles.containerHome}>
-         [Question 1 will be he
+        <Text style={pickerSelectStyles.question}>
+         Question 1 will be 
         </Text>
        <View style = {{marginLeft:10,marginTop: 10}} >
         
@@ -60,16 +57,23 @@ const Questionnaire = ({ navigation } : any) => {
         onValueChange={(value) => (value)}
         items={[
             { label: 'Yes', value: 'yes' },
-            { label: 'No', value: 'baseball' },
+            { label: 'No', value: 'no' },
         ]} />
         </View>
+     <TextInput style = {pickerSelectStyles.input}
+     multiline
+     numberOfLines={4}
+     placeholder= " Enter Description...">
 
+     </TextInput>
 
       <View style={styles.containerSignButton}>
         <TouchableOpacity style={[styles.loginButton, {backgroundColor: "blue"}]}>
           <Text style={[styles.loginTextButton, {color: "#FFFFFF"}]}>Submit</Text>
         </TouchableOpacity>
       </View>
+
+      
     </View>
     </View>
     </ScrollView>
@@ -77,19 +81,28 @@ const Questionnaire = ({ navigation } : any) => {
 };
 
 export default Questionnaire;
+
 const pickerSelectStyles = StyleSheet.create({
     inputIOS: {
       fontSize: 16,
+      borderTopEndRadius : 10,
+      borderBottomRightRadius:10,
+      borderBottomLeftRadius : 10,
+      borderTopLeftRadius:10,
       paddingVertical: 12,
       paddingHorizontal: 10,
       borderWidth: 1,
-      borderColor: 'gray',
+      borderColor: 'black',
       borderRadius: 4,
       color: 'black',
       paddingRight: 30, // to ensure the text is never behind the icon
     },
     inputAndroid: {
       fontSize: 16,
+      borderTopEndRadius : 10,
+      borderBottomRightRadius:10,
+      borderBottomLeftRadius : 10,
+      borderTopLeftRadius:10,
       paddingHorizontal: 10,
       paddingVertical: 8,
       borderWidth: 0.5,
@@ -98,5 +111,19 @@ const pickerSelectStyles = StyleSheet.create({
       color: 'black',
       paddingRight: 30, // to ensure the text is never behind the icon
     },
+    input: {
+          height: 100,
+          marginTop: 10,
+          marginLeft : 10,
+          borderWidth: 1,
+          padding: 10,
+          borderRadius : 10,
+          paddingTop: 12,
+          fontSize: 16,
+        },
+     question : {
+        marginHorizontal: 10,
+        paddingTop: 20,
+     }
   });
   
