97a4df60b98a6ab9cfc602f749e8c87b3e851be7
Belle-coder <ribelle.el.ayoubi@gmail.com>
2022-03-26 12:06:25 -0400

    making the courses pressable in the explore screen

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,11 +1,11 @@
 import React, {useRef, useState} from "react";
-import {View, ImageBackground} from "react-native";
+import {View, ImageBackground, TouchableOpacity, Pressable} from "react-native";
 import CardStack, {Card} from "react-native-card-stack-swiper";
 import {CardItem} from "../components";
 import styles from "../assets/styles";
 import DEMO from "../assets/data/demo";
 
-const Home = () => {
+const Home = ({navigation}: any) => {
     // const [swiper, setSwiper] = useState<CardStack | null>(null);
     const swiperRef = useRef(null);
     const onXPress = () => {
@@ -31,14 +31,16 @@ const Home = () => {
                 >
                     {DEMO.map((item) => (
                         <Card key={item.id}>
-                            <CardItem
-                                hasActions
-                                image={item.image}
-                                name={item.name}
-                                description={item.description}
-                                onHeartPress={onHeartPress}
-                                onXPress={onXPress}
-                            />
+                            <Pressable onPress={() => navigation.navigate("CourseProfile", item.id)}>
+                                <CardItem
+                                    hasActions
+                                    image={item.image}
+                                    name={item.name}
+                                    description={item.description}
+                                    onHeartPress={onHeartPress}
+                                    onXPress={onXPress}
+                                />
+                            </Pressable>
                         </Card>
                     ))}
                 </CardStack>
