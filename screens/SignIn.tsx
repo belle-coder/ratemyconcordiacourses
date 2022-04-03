7f11cb35b92d1c64ddb04ffdfd311853954b78ba
Mohammad Ali Zahir <m.alizahir786@gmail.com>
2022-04-03 01:58:53 -0400

    Disabled warnings

--- screens/SignIn.tsx
+++ screens/SignIn.tsx
@@ -1,76 +1,100 @@
 import { useNavigation } from "@react-navigation/native";
-import React ,{useEffect, useState}from "react";
-import { Text, TouchableOpacity, ImageBackground, View, TextInput, SafeAreaView } from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+  Text,
+  TouchableOpacity,
+  ImageBackground,
+  View,
+  TextInput,
+  SafeAreaView,
+} from "react-native";
 import styles from "../assets/styles";
 import { auth } from "../backend/firebase";
+import { LogBox } from "react-native";
 
-function SignIn({navigation}: any) {
+LogBox.ignoreLogs(["Setting a timer"]);
 
-  const [isSecureEntry, setIsSecureEntry] = React.useState(true);  
+function SignIn({ navigation }: any) {
+  const [isSecureEntry, setIsSecureEntry] = React.useState(true);
 
-  const [ email,setEmail]  = useState('');
-  const [ password,setPassword]  = useState('');
-  
-  const handleSignup=() => {
-    auth.signInWithEmailAndPassword(email,password)
-    .then((userCredentials: { user: any; }) => {
-      const user = userCredentials.user;
-      console.log('logged in with ',user.email);
-      navigation.navigate('Tab');
-    }).catch((error: { message: any; }) => alert(error.message))
-  }
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
+  const handleSignup = () => {
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then((userCredentials: { user: any }) => {
+        const user = userCredentials.user;
+        console.log("logged in with ", user.email);
+        navigation.navigate("Tab");
+      })
+      .catch((error: { message: any }) => alert(error.message));
+  };
 
-  
-  
-  const validateEmail =  (email: any) => {
-    const regexp = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const validateEmail = (email: any) => {
+    const regexp =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return regexp.test(email);
   };
 
- 
-    return(
-        <ImageBackground source={require("../assets/images/bg.png")}
-        style={styles.bg}>
-    <View style={[styles.containerHome, styles.containerSignIn]}>
-      <Text style={styles.signIntitle}>Sign In</Text>
-      <SafeAreaView>
-        <TextInput
-          placeholder="Email"
-          style={[styles.signIninput, {marginTop: 20}]}
-          onChangeText = {(text) => {setEmail(text), validateEmail(text)}}
-          value={email}
-        />
-      </SafeAreaView>
+  return (
+    <ImageBackground
+      source={require("../assets/images/bg.png")}
+      style={styles.bg}
+    >
+      <View style={[styles.containerHome, styles.containerSignIn]}>
+        <Text style={styles.signIntitle}>Sign In</Text>
+        <SafeAreaView>
+          <TextInput
+            placeholder="Email"
+            style={[styles.signIninput, { marginTop: 20 }]}
+            onChangeText={(text) => {
+              setEmail(text), validateEmail(text);
+            }}
+            value={email}
+          />
+        </SafeAreaView>
 
-      <SafeAreaView>
-        <TextInput
-            placeholder = "Password"
+        <SafeAreaView>
+          <TextInput
+            placeholder="Password"
             style={styles.signIninput}
-            onChangeText = {(text) => setPassword(text)}
-            secureTextEntry = {isSecureEntry}
+            onChangeText={(text) => setPassword(text)}
+            secureTextEntry={isSecureEntry}
             value={password}
           />
-      </SafeAreaView>
+        </SafeAreaView>
 
-      <View style={styles.containerSignButton}>
-        <TouchableOpacity style={[styles.loginButton, {backgroundColor: "#D2132A"}]} 
-        onPress = {handleSignup}>
-          <Text style={[styles.loginTextButton, {color: "#FFFFFF"}]}>Log in</Text>
-        </TouchableOpacity>
+        <View style={styles.containerSignButton}>
+          <TouchableOpacity
+            style={[styles.loginButton, { backgroundColor: "#D2132A" }]}
+            onPress={handleSignup}
+          >
+            <Text style={[styles.loginTextButton, { color: "#FFFFFF" }]}>
+              Log in
+            </Text>
+          </TouchableOpacity>
+        </View>
+
+        <View
+          style={[
+            styles.containerSignButton,
+            { marginTop: 20, marginBottom: 20 },
+          ]}
+        >
+          <Text style={{ color: "#757E90" }}>Don't have an account ? </Text>
+          <TouchableOpacity
+            style={{ marginLeft: 5 }}
+            onPress={() => navigation.navigate("SignUp")}
+          >
+            <Text style={[styles.loginTextButton, { color: "#D2132A" }]}>
+              Sign up
+            </Text>
+          </TouchableOpacity>
+        </View>
       </View>
-
-                
-
-                <View style={[styles.containerSignButton, {marginTop: 20, marginBottom: 20}]}>
-                    <Text style={{color: "#757E90"}}>Don't have an account ? </Text>
-                    <TouchableOpacity style={{marginLeft: 5}} onPress={() => navigation.navigate('SignUp')}>
-                        <Text style={[styles.loginTextButton, {color: "#D2132A"}]}>Sign up</Text>
-                    </TouchableOpacity>
-                </View>
-            </View>
-
-        </ImageBackground>    )
-};
+    </ImageBackground>
+  );
+}
 
 export default SignIn;
--- screens/SignUp.tsx
+++ screens/SignUp.tsx
@@ -1,63 +1,75 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 
-import { Text, TouchableOpacity, ImageBackground, View, TextInput, SafeAreaView } from "react-native";
+import {
+  Text,
+  TouchableOpacity,
+  ImageBackground,
+  View,
+  TextInput,
+  SafeAreaView,
+} from "react-native";
 import styles from "../assets/styles";
 import { auth } from "../backend/firebase";
+import { LogBox } from 'react-native';
 
-function SignUp({ navigation } : any) {
+LogBox.ignoreLogs(['Setting a timer']);
+
+function SignUp({ navigation }: any) {
   // const [values, setValues] = React.useState({email: '', password: '', password2: ''});
-  const [isSecureEntry, setIsSecureEntry] = React.useState(true);  
+  const [isSecureEntry, setIsSecureEntry] = React.useState(true);
 
-  const [ email,setEmail]  = useState('');
-  const [ password,setPassword]  = useState('');
-  
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
+  const handleSignup = () => {
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((userCredentials: { user: any }) => {
+        const user = userCredentials.user;
+        console.log(user.email);
+        navigation.navigate("SignIn"r);
+      })
+      .catch((error: { message: any }) => alert(error.message));
+  };
 
-  const handleSignup=() => {
-    auth.createUserWithEmailAndPassword(email,password)
-    .then((userCredentials: { user: any; }) => {
-      const user = userCredentials.user;
-      console.log(user.email);
-      navigation.navigate('Tab')
-    }).catch((error: { message: any; }) => alert(error.message))
-  }
-  
-  const validateEmail =  (email: any) => {
-    const regexp = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const validateEmail = (email: any) => {
+    const regexp =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return regexp.test(email);
   };
 
-  return(
+  return (
     <ImageBackground
       source={require("../assets/images/bg.png")}
       style={styles.bg}
     >
+      <View style={[styles.containerHome, styles.containerSignIn]}>
+        <Text style={styles.signIntitle}>Create an Account</Text>
+        <SafeAreaView>
+          <TextInput
+            placeholder="Email"
+            style={[styles.signIninput, { marginTop: 20 }]}
+            onChangeText={(text) => {
+              setEmail(text), validateEmail(text);
+            }}
+            value={email}
+          />
+        </SafeAreaView>
 
-    <View style={[styles.containerHome, styles.containerSignIn]}>
-      <Text style={styles.signIntitle}>Create an Account</Text>
-      <SafeAreaView>
-        <TextInput
-          placeholder="Email"
-          style={[styles.signIninput, {marginTop: 20}]}
-          onChangeText = {(text) => {setEmail(text), validateEmail(text)}}
-          value={email}
-        />
-      </SafeAreaView>
-
-      <SafeAreaView>
-        <TextInput
-            placeholder = "Password"
+        <SafeAreaView>
+          <TextInput
+            placeholder="Password"
             style={styles.signIninput}
-            onChangeText = {(text) => setPassword( text)}
-            secureTextEntry = {isSecureEntry}
+            onChangeText={(text) => setPassword(text)}
+            secureTextEntry={isSecureEntry}
             value={password}
           />
-          <Text style= {{marginStart:20}}>Password must be at least 8 characters</Text>
-        
-       
-      </SafeAreaView>
+          <Text style={{ marginStart: 20 }}>
+            Password must be at least 8 characters
+          </Text>
+        </SafeAreaView>
 
-      {/* <SafeAreaView>
+        {/* <SafeAreaView>
         <TextInput
             placeholder = "Re-enter password"
             style={styles.signIninput}
@@ -68,23 +80,36 @@ function SignUp({ navigation } : any) {
         {values.password !== values.password2 && values.password2 !== '' && <Text style={{color: "red", marginLeft: 20}}>Passwords don't match. Try again.</Text>}
       </SafeAreaView> */}
 
-      <View style={styles.containerSignButton}>
-        <TouchableOpacity style={[styles.loginButton, {backgroundColor: "#D2132A"}]} 
-        onPress={handleSignup}
-        >
-          <Text style={[styles.loginTextButton, {color: "#FFFFFF"}]}>Sign up</Text>
-        </TouchableOpacity>
-      </View>
-
-      <View style={[styles.containerSignButton, {marginTop: 20, marginBottom: 20}]}>
-        <Text style={[{color: "#757E90"}]}>Already have an account ? </Text>
-        <TouchableOpacity style={[{marginLeft: 5}]} onPress={() => navigation.navigate('SignIn')}>
-            <Text style={[styles.loginTextButton, {color: "#D2132A"}]}>Sign in</Text>
+        <View style={styles.containerSignButton}>
+          <TouchableOpacity
+            style={[styles.loginButton, { backgroundColor: "#D2132A" }]}
+            onPress={handleSignup}
+          >
+            <Text style={[styles.loginTextButton, { color: "#FFFFFF" }]}>
+              Sign up
+            </Text>
           </TouchableOpacity>
-      </View>
-    </View>
+        </View>
 
+        <View
+          style={[
+            styles.containerSignButton,
+            { marginTop: 20, marginBottom: 20 },
+          ]}
+        >
+          <Text style={[{ color: "#757E90" }]}>Already have an account ? </Text>
+          <TouchableOpacity
+            style={[{ marginLeft: 5 }]}
+            onPress={() => navigation.navigate("SignIn")}
+          >
+            <Text style={[styles.loginTextButton, { color: "#D2132A" }]}>
+              Sign in
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </ImageBackground>
-)};
+  );
+}
 
 export default SignUp;
